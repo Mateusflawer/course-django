39fccff08674f59ad6da3c7f508cb49b04a80461
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2024-09-23 20:37:51 -0300

    Iniciando denovo

--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def blog(request):
-    return render(request, "blog/index.html")
-
-def exemplo(request):
-    return render(request, "blog/exemplo.html")
