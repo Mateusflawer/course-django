f7ab043392d5cace05cae5f27c2f1d4369ec7df2
Mateus Alves <111348801+Mateusflawer@users.noreply.github.com>
2024-10-02 21:14:16 -0300

    Aula de outro dia

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 
 def blog(request):
+    context = {
+        'title': 'Blog',
+        'text': 'Olá blog!',
+    }
+    
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context,
     ) 
 
+
 def exemplo(request):
+    context = {
+        'title': 'Exemplo',
+        'text': 'Olá exemplo!',
+    }
+    
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context
     )
--- home/views.py
+++ home/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    
+    context = {
+        'title': 'Home',
+        'text': 'Olá home!'
+    }
+    
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
     )
